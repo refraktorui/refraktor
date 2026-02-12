13b2577a34976c881353259886c5c4315a26294f
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2026-02-12 22:59:54 +0200

    feat: add Check and Minus icons

--- packages/core/src/icons/index.ts
+++ packages/core/src/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as ChevronIcon } from "./chevron";
+export { default as CheckIcon } from "./check";
+export { default as MinusIcon } from "./minus";
 export { default as UserIcon } from "./user";
