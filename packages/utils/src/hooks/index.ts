4ee9c82c2369cadaf12c5fd3942464b6ec338410
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2026-02-13 01:00:51 +0200

    feat: add use-debounced-callback hook

--- packages/utils/src/hooks/index.ts
+++ packages/utils/src/hooks/index.ts
@@ -3,3 +3,4 @@ export { useDisclosure } from "./use-disclosure";
 export { useClickOutside } from "./use-click-outside";
 export { useMergedRefs } from "./use-merged-refs";
 export { useUncontrolled } from "./use-uncontrolled";
+export { useDebouncedCallback } from "./use-debounced-callback";
