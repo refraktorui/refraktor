dd0b84c06c19713eea15e863c80c070c93585e38
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2026-02-13 00:45:26 +0200

    feat: add use-click-outside hook

--- packages/utils/src/hooks/index.ts
+++ packages/utils/src/hooks/index.ts
@@ -1,4 +1,5 @@
 export { useId } from "./use-id";
 export { useDisclosure } from "./use-disclosure";
+export { useClickOutside } from "./use-click-outside";
 export { useMergedRefs } from "./use-merged-refs";
 export { useUncontrolled } from "./use-uncontrolled";
