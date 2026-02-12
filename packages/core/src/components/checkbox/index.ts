d4be94521aaa8cfd6805c57feb7f57c1b09072af
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2026-02-12 23:00:07 +0200

    feat: add Checkbox component

---
+++ packages/core/src/components/checkbox/index.ts
@@ -0,0 +1,10 @@
+export { default as Checkbox } from "./checkbox";
+export { CheckboxGroup } from "./checkbox-group";
+export type {
+    CheckboxProps,
+    CheckboxClassNames,
+    CheckboxGroupProps,
+    CheckboxGroupClassNames,
+    CheckboxLabelPosition,
+    CheckboxGroupOrientation
+} from "./checkbox.types";
--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./avatar";
 export * from "./badge";
 export * from "./button";
+export * from "./checkbox";
 export * from "./input";
 export * from "./loader";
 export * from "./menu";
