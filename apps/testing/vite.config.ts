7a2be78a145a6a1d664f1c9f598b70e213003140
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2026-02-14 20:19:50 +0200

    fix: exclude include on vite config

--- apps/testing/vite.config.ts
+++ apps/testing/vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     plugins: [react(), tailwindcss()],
     base: "./",
     optimizeDeps: {
-        include: ["@refraktor/core", "react", "react-dom"]
+        exclude: ["@refraktor/core", "@refraktor/utils"],
+        include: ["react", "react-dom"]
     },
     build: {
         outDir: "build"
