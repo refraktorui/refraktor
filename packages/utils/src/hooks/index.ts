e61598105292ed473fb0ba12c2563369e20003fd
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2026-02-15 20:08:28 +0200

    feat: add use-isomorpic-layout-effect hook

--- packages/utils/src/hooks/index.ts
+++ packages/utils/src/hooks/index.ts
@@ -5,3 +5,4 @@ export { useResizeObserver } from "./use-resize-observer";
 export { useMergedRefs } from "./use-merged-refs";
 export { useUncontrolled } from "./use-uncontrolled";
 export { useDebouncedCallback } from "./use-debounced-callback";
+export { useIsomorphicLayoutEffect } from "./use-isomorphic-layout-effect";
