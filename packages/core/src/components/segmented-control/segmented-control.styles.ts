612593cbedb89c440b028166deb1a61d93a1ef86
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2026-02-23 01:10:10 +0200

    chore: update segmentedcontrol sizing to more match inputs

--- packages/core/src/components/segmented-control/segmented-control.styles.ts
+++ packages/core/src/components/segmented-control/segmented-control.styles.ts
@@ -9,28 +9,28 @@ type SegmentedControlSizeStyles = {
 const sizes: Record<RefraktorSize, SegmentedControlSizeStyles> = {
     xs: {
         root: "p-0.5 gap-0.5",
-        control: "h-6 px-2 min-w-7",
-        label: "text-[10px]"
+        control: "h-4 px-2 min-w-6",
+        label: "text-[8px]"
     },
     sm: {
         root: "p-0.5 gap-0.5",
-        control: "h-7 px-2.5 min-w-8",
-        label: "text-xs"
+        control: "h-5 px-2.5 min-w-7",
+        label: "text-[10px]"
     },
     md: {
-        root: "p-1 gap-1",
-        control: "h-8 px-3 min-w-9",
-        label: "text-sm"
+        root: "p-0.5 gap-0.5",
+        control: "h-7 px-3 min-w-8",
+        label: "text-xs"
     },
     lg: {
-        root: "p-1 gap-1",
-        control: "h-9 px-3.5 min-w-10",
-        label: "text-base"
+        root: "p-0.5 gap-0.5",
+        control: "h-9 px-3.5 min-w-9",
+        label: "text-sm"
     },
     xl: {
-        root: "p-1.5 gap-1.5",
-        control: "h-10 px-4 min-w-11",
-        label: "text-lg"
+        root: "p-0.5 gap-0.5",
+        control: "h-11 px-4 min-w-10",
+        label: "text-base"
     }
 };
 
