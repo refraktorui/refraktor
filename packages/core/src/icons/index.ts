a18dfd9a47e50c3229f12ee9b16cfd1d03b9f61c
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2026-02-22 02:01:12 +0200

    feat: add eye and eye-off icons

--- packages/core/src/icons/index.ts
+++ packages/core/src/icons/index.ts
@@ -1,5 +1,7 @@
 export { default as ChevronIcon } from "./chevron";
 export { default as CheckIcon } from "./check";
+export { default as EyeIcon } from "./eye";
+export { default as EyeOffIcon } from "./eye-off";
 export { default as MinusIcon } from "./minus";
 export { default as UserIcon } from "./user";
 export { default as XIcon } from "./x";
